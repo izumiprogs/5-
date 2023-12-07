939088557e8ecf260ac5a08c26f0c3496e3805c1
Ярослав <146277976+izumiprogs@users.noreply.github.com>
2023-12-07 11:46:28 +0300

    /Первое задание

---
+++ 1 задание/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Math;
+
+
+namespace _5_1_4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void Button_Click(object sender, EventArgs e)
+        {
+            int A = Convert.ToInt32(Rezult_a.Text);
+            int B = Convert.ToInt32(Rezult_b.Text);
+            if (A > B) { A = A / B; }
+            else { TextBlockAnswer.Text = $"вы ввели числа неправильно"; }
+            TextBlockAnswer.Text = $"{A}";
+        }
+    }
+}
