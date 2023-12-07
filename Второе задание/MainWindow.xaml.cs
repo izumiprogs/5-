e07a698228d7d4c00202db92447d4fc99b4a48b3
Ярослав <146277976+izumiprogs@users.noreply.github.com>
2023-12-07 13:52:58 +0300

    Второе задание

---
+++ Второе задание/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Math;
+
+namespace _2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void Button_Click(object sender, EventArgs e)
+        {
+            int N = Convert.ToInt32(Rezult_a.Text);
+            N = N % 3600;
+            TextBlockAnswer.Text = $"{N}";
+        }
+    }
+}
